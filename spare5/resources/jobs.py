a9b7c6447cbb2a534746af72ef21f5b5a138851b
Philip Kimmey <philip@rover.com>
2015-03-24 11:09:35 -0700

    Move list functionality into base class

--- spare5/resources/jobs.py
+++ spare5/resources/jobs.py
@@ -14,14 +14,5 @@ class Jobs(ListResource):
     def url(self):
         return '/'.join((self.client.api_root, 'jobs'))
 
-    def create(self, data):
-        """
-        Since the API for jobs is minimally defined, keep it flexible for now.
-        """
-        self.client._post(self.url, data)
-
-    def list(self):
-        self.client._get(self.url)
-
     def get(self, reference_id):
         pass
